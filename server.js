029de66a044f7d347242e54fa4d3dc3fa822b72d
MukundaHolla <kanda05holla@gmail.com>
2019-10-12 21:08:38 +0530

    fix

--- server.js
+++ server.js
@@ -4,31 +4,28 @@ const path = require('path');
 
 const app = express();
 
-// connect database
+// Connect Database
 connectDB();
 
-// Init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => res.send('API running'));
-
-// Define routes
-
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if ((process.env.NODE_ENV = 'production')) {
-  // set static folder
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'client', 'built', 'index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started in the port- ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
